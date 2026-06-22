4eb70627747c7c08f1626ed4e449e22177d8f3df
Mridul <mriduls.brainerhub@gmail.com>
2026-06-22 15:42:48 +0530

    Fix jwt authentication setup

--- app/core/auth.py
+++ app/core/auth.py
@@ -34,11 +34,11 @@ def create_access_token(data: dict):
 
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(
+    encoded_jwt = jwt.encode( 
         to_encode,
         SECRET_KEY,
         algorithm=ALGORITHM
-    )
+    ) 
 
     return encoded_jwt
 
