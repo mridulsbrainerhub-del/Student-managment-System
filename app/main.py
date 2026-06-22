688c55d643a668b77f23f4ae97f9917da9182059
Mridul <mriduls.brainerhub@gmail.com>
2026-06-22 17:14:32 +0530

    main class updated

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.core import student,users,teacher,school_class
 from app.routers.student import router as student_router
 from app.routers.auth import router as auth_router
 from app.routers.teacher import router as teacher_router
-
+from app.routers.school_class import router as school_class_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -16,6 +16,7 @@ app = FastAPI(title="Study Buddy Club")
 app.include_router(auth_router)
 app.include_router(student_router)
 app.include_router(teacher_router)
+app.include_router(school_class_router)
 @app.get("/")
 def home():
     return {"message": "Student Management System API is running"}
